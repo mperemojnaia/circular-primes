9c193fdab0ce42ab7c7ec2762a00ef19a323f911
mperemojnaia <marina.peremojnaia@gmail.com>
2017-02-23 19:00:45 +0200

    circular primes

---
+++ src/main/java/circularprimes/MainApplication.java
@@ -0,0 +1,11 @@
+package circularprimes;
+
+public class MainApplication {
+    
+    public static void main(String args[]){
+        long mln = 100;
+        
+        System.out.println(CircularPrimes.findCircularPrimes(mln));
+
+    }
+}
